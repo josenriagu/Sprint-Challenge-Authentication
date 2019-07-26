382fddd6814b90c169d4eeeced6862f2e3501e17
Josemaria <josemarianriagu25@gmail.com>
2019-07-26 18:23:06 +0800

    implement signup process

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
-  nameRef = React.createRef()
-  passwordRef = React.createRef()
+  nameRef = React.createRef();
+  passwordRef = React.createRef();
 
+  onSubmit = event => {
+    event.preventDefault()
+    const user = {
+      username: this.nameRef.current.value,
+      password: this.passwordRef.current.value
+    }
+    axios.post('http://localhost:3300/api/auth/register', user)
+      .then(res => {
+        alert(res.data.message)
+        window.location.href = "http://localhost:3000/login"
+      }).catch(err => {
+        console.log(err)
+      })
+  }
 
-  
   render() {
     return (
       <div className="App" >
-        <form>
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             required
