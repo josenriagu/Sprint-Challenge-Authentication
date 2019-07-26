a9c6245986e4ef378ff27e1c6158e3c85a5767dc
Josemaria <josemarianriagu25@gmail.com>
2019-07-26 17:00:11 +0800

    define knex configurations

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,34 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' }, // change this if you want a different name for the database
+    connection: {
+      filename: './database/user.db3'
+    }, // change this if you want a different name for the database
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool: { // required if we are going to have foreign keys on any of our tables
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 };
